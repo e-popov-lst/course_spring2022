a71d8dfee866a6c6b329be304f83fd2697ba4e18
Eugene Popov <eugene.popov@list.ru>
2022-12-02 00:30:58 +0300

    lesson 1.6 - 1 part

--- ru.popov.springcourse/src/main/java/ru/popov/springcourse/MusicPlayer.java
+++ ru.popov.springcourse/src/main/java/ru/popov/springcourse/MusicPlayer.java
@@ -2,10 +2,33 @@ package ru.popov.springcourse;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
 
-    // IoC
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+    public MusicPlayer() {}
+
+    // Ioc
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 
     public void playMusic() {
